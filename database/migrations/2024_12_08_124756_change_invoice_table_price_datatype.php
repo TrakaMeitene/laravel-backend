08239c8e6cde51ada280f2939556f9d498e44be0
Sandra Jurberga-Šaudine <sandra.jurberga@gmail.com>
2024-12-08 12:54:54 +0200

    issue nr. 14

--- database/migrations/2024_12_08_124756_change_invoice_table_price_datatype.php
+++ database/migrations/2024_12_08_124756_change_invoice_table_price_datatype.php
@@ -11,12 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('invoice', function (Blueprint $table) {
-          
-            $table->string( 'service');
-            $table->integer( 'price');
-
-        });
+  
+            Schema::table('invoice', function (Blueprint $table) {
+              
+                $table->bigInteger(column: 'price')->change();
+    
+            });
+    
     }
 
     /**

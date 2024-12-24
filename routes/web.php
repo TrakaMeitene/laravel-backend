f2e671e829da954b1a7d6b447fe493dde01760c5
Ivars <ivars@ivars.lv>
2024-12-24 14:10:55 +0200

    remove dead code

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,10 @@
 <?php
 
-use App\Mail\Booking;
-use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PaymentController;
 
 Route::get('/', function () {
     return '<a href="https://pierakstspie.lv">PierakstsPie.lv</a>';
-});
-
-Route::get('send-mail', function () {
-    $details = [
-        'title' => 'Success',
-        'content' => 'This is an email testing using Laravel',
-    ];
-   //return view('emails.email', ['content' => '19.11.2024 plkst. 8:00, Sandra jurberga-Šaudine. O.kalapka iela, Liepāja ']);
-    Mail::to('sandra.jurberga@gmail.com')->send(new Booking($details));
-
-
-    return 'Email sent at ' . now();
 });
 
 Route::get('/success', action: [PaymentController::class, 'success'])->name('success-route');
